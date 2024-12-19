239ee7ad4347a472234da51fc31be0fb799b25ee
Norbert Baranya <baranya.norbert@gmail.com>
2024-12-19 12:16:28 +0100

    init

---
+++ src/main/java/com/example/duration_formatter/DurationFormatterApplication.java
@@ -0,0 +1,13 @@
+package com.example.duration_formatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurationFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DurationFormatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/duration_formatter/DurationFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duration_formatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurationFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
